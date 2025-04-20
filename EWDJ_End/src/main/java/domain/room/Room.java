9544d6aeef4c5b7e009829f56d4aa45b147413eb
Guillaume Desktop <robertohelper@outlook.com>
2025-04-20 18:08:52 +0200

    feature: added some room validation

--- EWDJ_End/src/main/java/domain/room/Room.java
+++ EWDJ_End/src/main/java/domain/room/Room.java
@@ -1,12 +1,17 @@
 // well it's technically a class but to avoid confusion I've named it room
 package domain.room;
 
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class Room {
+	@Pattern(regexp = "^[a-zA-Z]\\d{3}$")
 	private String name;
+	@Range(min = 1, max = 50)
 	private Integer capacity;
 }
