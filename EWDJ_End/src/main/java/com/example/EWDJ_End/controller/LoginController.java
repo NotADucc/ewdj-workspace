0b2c8a3eafd38f26c57bcd35f2079aca58a0d67f
Guillaume Desktop <robertohelper@outlook.com>
2025-05-09 13:54:01 +0200

    minor

--- EWDJ_End/src/main/java/com/example/EWDJ_End/controller/LoginController.java
+++ EWDJ_End/src/main/java/com/example/EWDJ_End/controller/LoginController.java
@@ -4,21 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    
-    @GetMapping
-    public String login(String error, String logout, Model model) {
 
-        if (error != null) {
-            model.addAttribute("error", "Invalid username and password!");
-        }
-        if (logout != null) {
-            model.addAttribute("msg", "You've been logged out successfully.");
-        }
-        return "login";
-    }
+	@GetMapping
+	public String login(
+			@RequestParam(required = false) String error,
+			@RequestParam(required = false) String logout,
+			Model model
+	) {
+
+		if (error != null) {
+			model.addAttribute("error", "Invalid username and password!");
+		}
+		if (logout != null) {
+			model.addAttribute("msg", "You've been logged out successfully.");
+		}
+		return "login";
+	}
 
 }
