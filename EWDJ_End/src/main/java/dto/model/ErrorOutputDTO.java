b3ad47cf0433b4ab641a3b567fe6ab7e753f97ab
Guillaume Desktop <robertohelper@outlook.com>
2025-05-16 16:32:24 +0200

    fix: class/record error

--- EWDJ_End/src/main/java/dto/model/ErrorOutputDTO.java
+++ EWDJ_End/src/main/java/dto/model/ErrorOutputDTO.java
@@ -1,14 +1,4 @@
 package dto.model;
 
-import lombok.Getter;
-
-@Getter
-public record ErrorOutputDTO {
-	private String message;
-	private int status;
-
-	public ErrorOutputDTO(String message, int status) {
-		this.message = message;
-		this.status = status;
-	}
+public record ErrorOutputDTO(String message, int status) {
 }
--- EWDJ_End/src/test/java/com/springBoot/EWDJ_End/config/ConferenceConfigMock.java
+++ EWDJ_End/src/test/java/com/springBoot/EWDJ_End/config/ConferenceConfigMock.java
@@ -8,22 +8,23 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.validation.Validator;
 
 @TestConfiguration
 public class ConferenceConfigMock {
 	
-    @Bean
+    @Bean @Primary
     LocalDateTime conferenceRangeStart() {
         return LocalDateTime.of(2020, 1, 1, 1, 0);
     }
     
-    @Bean
+    @Bean @Primary
     LocalDateTime conferenceRangeEnd() {
         return LocalDateTime.of(2021, 1, 1, 1, 0);
     }
 
-    @Bean
+    @Bean @Primary
     Validator conferenceRangeValidator() {
         Validator validator = mock(Validator.class);
         when(validator.supports(any())).thenReturn(true);
